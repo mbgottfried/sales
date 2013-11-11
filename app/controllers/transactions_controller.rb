92792ab33de72453704fa094667dd3ef98d2fe1e
shagmbg <michael.b.gottfried@gmail.com>
2013-11-11 15:02:31 -0500

    cleaned up transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,52 +3,49 @@ class TransactionsController < ApplicationController
     only: [:new, :create]
 
   def new
-    @product = Product.find_by!(
-      permalink: params[:permalink]
-    )
-  end
+@product = Product.find_by!(
+permalink: params[:permalink]
+)
+end
 
-  def show
-    @sale = Sale.find_by!(guid: params[:guid])
-    @product = @sale.product
-  end
+def show
+@sale = Sale.find_by!(guid: params[:guid])
+@product = @sale.product
+end
 
-  def create
-    product = Product.find_by!(
-      permalink: params[:permalink]
-    )
+def create
+product = Product.find_by!(
+permalink: params[:permalink]
+)
 
-    token = params[:stripeToken]
+token = params[:stripeToken]
 
-    begin
-      charge = Stripe::Charge.create(
-        amount:      product.price,
-        currency:    "usd",
-        card:        token,”
-        description: params[:email]
-      )
-      @sale = product.sales.create!(
-        product_id: product.id,
-        email:      params[:email]
-      )
-      redirect_to pickup_url(guid: @sale.guid)
-    rescue Stripe::CardError => e
-      # The card has been declined or
-      # some other error has occured
-      @error = e
-      render :new
-    end
-  end
+begin
+charge = Stripe::Charge.create(
+amount: product.price,
+currency: "usd",
+card: token,
+description: params[:email]
+)
+@sale = product.sales.create!(
+product_id: product.id,
+email: params[:email]
+)
+redirect_to pickup_url(guid: @sale.guid)
+rescue Stripe::CardError => e
+# The card has been declined or
+# some other error has occured
+@error = e
+render :new
+end
+end
 
-  def download
-    @sale = Sale.find_by!(guid: params[:guid])
-
-    resp = HTTParty.get(@sale.product.file.url)
-
-    filename = @sale.product.file.url
-    send_data resp.body,
-      :filename => File.basename(filename),
-      :content_type => resp.headers['Content-Type']
-  end
-
+def download
+@sale = Sale.find_by!(guid: params[:guid])
+resp = HTTParty.get(@sale.product.file.url)
+filename = @sale.product.file.url
+send_data resp.body,
+:filename => File.basename(filename),
+:content_type => resp.headers['Content-Type']
+end
 end
